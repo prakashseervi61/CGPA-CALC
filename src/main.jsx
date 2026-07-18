0c231a5773a6345aa56a550454dbbf69bd350e14
Prakash V <prakashseervi1503@gmail.com>
2026-07-18 08:11:40 +0530

    fix: hardcode BrowserRouter basename for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/CGPA-CALC">
       <AuthProvider>
         <DataProvider>
           <ThemeProvider>
